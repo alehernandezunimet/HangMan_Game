4d2ca1bfdb5fc811905728cdd7bbda6ea1b6a327
AleH <alehernandez@correo.unimet.edu.ve>
2025-09-29 23:50:23 -0400

    screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
 
-
-
 void main() {
   // Inicialización para plugins como SharedPreferences
-  WidgetsFlutterBinding.ensureInitialized(); 
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const HangmanApp());
+}
+
+// Widget Raíz (StatelessWidget)
+class HangmanApp extends StatelessWidget {
+  const HangmanApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "El Ahorcado",
+      // Desactivar el banner de "DEBUG"
+      debugShowCheckedModeBanner: false,
+
+      // Tema oscuro para el contraste
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.blueGrey,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+
+      // Pantalla Inicial
+      home: const GameScreen(),
+    );
+  }
 }
